be4f498b5434dfd4e0678fed36bdcaec5f1d6126
ceclinux <src655@gmail.com>
2014-09-11 14:01:29 +0000

    histo and togray

---
+++ histo.m
@@ -0,0 +1,13 @@
+%draw the histogram of the grayscale image
+im = imread('ff.jpg')
+%the size of im is m*n resolution
+[m, n] = size(im)
+%fill the matrix with 256 rows and 1 column
+h = zeros(256, 1)
+for i = 1:m
+    for j = 1:n
+        c = im(i, j)
+        h(c+1) = h(c+1)+1
+    end
+end
+plot(h)
---
+++ togrey.m
@@ -0,0 +1,11 @@
+%convert a image to grayscale image
+im = imread('fight.jpg')
+# A(:, l) is the l th column of matrix A
+# A(k, :) is the k th row of matrix A
+red = double(im(:,:,1))
+green = double(im(:,:,2))
+blue = double(im(:,:,3))
+grey = 11*red/32 + 16*green/100 + 5*blue/100
+# convert to unsigned 8-bit integer type
+grey = uint8(round(grey))
+imwrite(grey, 'grey.jpg')
